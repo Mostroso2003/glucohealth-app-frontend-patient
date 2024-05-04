26c380cf973ee6546a56e827e6de30b8aa6bde39
Aurimart Garcia <aurimart.liliana@gmail.com>
2024-05-04 11:54:06 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'glucohealth-app-frontend-patient',
+  webDir: 'dist'
+};
+
+export default config;
